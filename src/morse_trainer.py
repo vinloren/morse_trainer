ff7287df510962042c495955c03a3df59c5c154d
Vincenzo Lorenzale <vinloren@gmail.com>
2021-08-09 22:29:58 +0200

    updated source

--- src/morse_trainer.py
+++ src/morse_trainer.py
@@ -1,5 +1,81 @@
 import socket
 import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget,  \
+            QVBoxLayout,QHBoxLayout,QLineEdit,QTextEdit,QLabel,QCheckBox, \
+            QPushButton,QRadioButton,QComboBox    
+
+
+class App(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.title = 'Morse Trainer'
+        self.initUI()
+
+    def initUI(self):
+        connbtn = QPushButton("CONNECT",self)
+        iplbl = QLabel("Host IP:")
+        portlbl = QLabel("Port:")
+        self.ipadr = QLineEdit()
+        self.port = QLineEdit()
+        self.ipadr.setText("192.168.1.5")
+        self.port.setText("8888")
+        voidlbl = QLabel("")
+        voidlbl.setMinimumWidth(5)
+        speedbtn = QPushButton("SET",self)
+        speedbtn.setMaximumWidth(50)
+        lblspeed = QLabel("Speed:")
+        self.cmbspeed = QComboBox(self)
+        self.cmbspeed.addItem('5wpm')
+        self.cmbspeed.addItem('10wpm')
+        self.cmbspeed.addItem('15wpm')
+        self.cmbspeed.addItem('20wpm')
+        self.cmbspeed.addItem('25wpm')
+        self.cmbspeed.addItem('30wpm')
+        tonebtn = QPushButton("SET",self)
+        tonebtn.setMaximumWidth(50)
+        lbltone = QLabel("Tone Hz:")
+        self.cmbtone = QComboBox(self)
+        self.cmbtone.addItem('400')
+        self.cmbtone.addItem('500')
+        self.cmbtone.addItem('600')
+        self.cmbtone.addItem('700')
+        self.cmbtone.addItem('800')
+        self.cmbtone.addItem('900')
+        hhome = QHBoxLayout()
+        hhome.addWidget(connbtn)
+        hhome.addWidget(iplbl)
+        self.ipadr.setMaximumWidth(80)
+        hhome.addWidget(self.ipadr)
+        hhome.addWidget(portlbl)
+        self.port.setMaximumWidth(45)
+        hhome.addWidget(self.port)
+        hhome.addWidget(speedbtn)
+        hhome.addWidget(lblspeed)
+        hhome.addWidget(self.cmbspeed)
+        hhome.addWidget(tonebtn)
+        hhome.addWidget(lbltone)
+        hhome.addWidget(self.cmbtone)
+        hmed = QHBoxLayout()
+        xmtlbl = QLabel("Transmit")
+        rcvlbl = QLabel("Receive")
+        self.xmt = QTextEdit()
+        self.rcv = QTextEdit()
+        hmed.addWidget(xmtlbl)
+        hmed.addWidget(rcvlbl)
+        hbot = QHBoxLayout()
+        hbot.addWidget(self.xmt)
+        hbot.addWidget(self.rcv)
+        self.layout = QVBoxLayout(self)
+        self.layout.addLayout(hhome) 
+        self.layout.addLayout(hmed)
+        self.layout.addLayout(hbot)
+        sendbtn = QPushButton("SEND DATA",self)
+        self.layout.addWidget(sendbtn)
+        #hhome.addWidget(voidlbl)
+        self.setGeometry(100, 50, 620,400)
+        self.show()
+
 
 def invia_comandi(s):
     while True:
@@ -27,4 +103,7 @@ def conn_sub_server(indirizzo_server):
     invia_comandi(s)
 
 if __name__ == '__main__':
-    conn_sub_server(("192.168.1.5", 8888))
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())  
+    #conn_sub_server(("192.168.1.5", 8888))

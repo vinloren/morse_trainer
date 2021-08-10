953f5ee84f3256490bc9a71ac6e784766912b3ea
Vincenzo Lorenzale <vinloren@gmail.com>
2021-08-10 12:15:30 +0200

    updated source code

--- src/morse_trainer.py
+++ src/morse_trainer.py
@@ -6,14 +6,16 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget,  \
 
 
 class App(QWidget):
-    
+    s = socket.socket()
+    CONNECTED = False
     def __init__(self):
         super().__init__()
         self.title = 'Morse Trainer'
         self.initUI()
 
     def initUI(self):
-        connbtn = QPushButton("CONNECT",self)
+        self.connbtn = QPushButton("CONNECT",self)
+        self.connbtn.clicked.connect(self.connect_click)
         iplbl = QLabel("Host IP:")
         portlbl = QLabel("Port:")
         self.ipadr = QLineEdit()
@@ -23,6 +25,7 @@ class App(QWidget):
         voidlbl = QLabel("")
         voidlbl.setMinimumWidth(5)
         speedbtn = QPushButton("SET",self)
+        speedbtn.clicked.connect(self.set_speed_click)
         speedbtn.setMaximumWidth(50)
         lblspeed = QLabel("Speed:")
         self.cmbspeed = QComboBox(self)
@@ -33,6 +36,7 @@ class App(QWidget):
         self.cmbspeed.addItem('25wpm')
         self.cmbspeed.addItem('30wpm')
         tonebtn = QPushButton("SET",self)
+        tonebtn.clicked.connect(self.tone_click)
         tonebtn.setMaximumWidth(50)
         lbltone = QLabel("Tone Hz:")
         self.cmbtone = QComboBox(self)
@@ -43,7 +47,7 @@ class App(QWidget):
         self.cmbtone.addItem('800')
         self.cmbtone.addItem('900')
         hhome = QHBoxLayout()
-        hhome.addWidget(connbtn)
+        hhome.addWidget(self.connbtn)
         hhome.addWidget(iplbl)
         self.ipadr.setMaximumWidth(80)
         hhome.addWidget(self.ipadr)
@@ -63,6 +67,13 @@ class App(QWidget):
         self.rcv = QTextEdit()
         hmed.addWidget(xmtlbl)
         hmed.addWidget(rcvlbl)
+        hnbot = QHBoxLayout()
+        clearsnd = QPushButton("CLEAR",self)
+        clearrcv = QPushButton("CLEAR",self)
+        clearsnd.clicked.connect(self.clear_snd)
+        clearrcv.clicked.connect(self.clear_rcv)
+        hnbot.addWidget(clearsnd)
+        hnbot.addWidget(clearrcv)
         hbot = QHBoxLayout()
         hbot.addWidget(self.xmt)
         hbot.addWidget(self.rcv)
@@ -70,37 +81,77 @@ class App(QWidget):
         self.layout.addLayout(hhome) 
         self.layout.addLayout(hmed)
         self.layout.addLayout(hbot)
+        self.layout.addLayout(hnbot)
         sendbtn = QPushButton("SEND DATA",self)
+        sendbtn.clicked.connect(self.send_click)
         self.layout.addWidget(sendbtn)
         #hhome.addWidget(voidlbl)
         self.setGeometry(100, 50, 620,400)
         self.show()
 
+    def clear_snd(self):
+        self.xmt.clear()
 
-def invia_comandi(s):
-    while True:
-        comando = input("-> ")
+    def clear_rcv(self):
+        self.rcv.clear()
+
+    def connect_click(self):
+        if(self.CONNECTED == False):
+            host = self.ipadr.text()
+            port = int(self.port.text())
+            self.conn_sub_server((host,port))  
+        else:
+            self.invia_comandi("ESC") 
+
+    def send_click(self):
+        data = self.xmt.toPlainText()
+        riga = data.split("\n")
+        l = len(riga)
+        testo = riga[l-2]
+        print(testo) # stampa ultima riga (da ultimo \n in poi)
+        self.invia_comandi(testo)
+
+
+    def set_speed_click(self):
+        speed = self.cmbspeed.currentText()
+        print(speed)
+        self.invia_comandi(speed)
+
+
+    def tone_click(self):
+        tone = self.cmbtone.currentText()
+        print("Tone Hz: "+tone )
+        self.invia_comandi(tone)
+
+
+    def conn_sub_server(self,indirizzo_server):
+        try:
+            #s = socket.socket()             # creazione socket client
+            self.s.connect(indirizzo_server)     # connessione al server
+            print(f"Connessessione al Server: { indirizzo_server } effettuata.")
+            self.CONNECTED = True
+            self.connbtn.setText("CLOSE")
+        except socket.error as errore:
+            print(f"Qualcosa è andato storto, sto uscendo... \n{errore}")
+            sys.exit()
+        data = self.s.recv(4096)
+        print(str(data, "utf-8"))
+        self.rcv.append(str(data,"utf-8"))
+
+
+    def invia_comandi(self,comando):
         if comando == "ESC":
             print("Sto chiudendo la connessione col Server.")
-            s.close()
+            self.s.close()
             sys.exit()
         else:
             comando = comando+"\r\n"
-            s.send(comando.encode())
-            data = s.recv(4096)
+            self.s.send(comando.encode())
+            data = self.s.recv(4096)
+            self.rcv.append(str(data,"utf-8"))
             print(str(data, "utf-8"))
+            
 
-def conn_sub_server(indirizzo_server):
-    try:
-        s = socket.socket()             # creazione socket client
-        s.connect(indirizzo_server)     # connessione al server
-        print(f"Connessessione al Server: { indirizzo_server } effettuata.")
-    except socket.error as errore:
-        print(f"Qualcosa è andato storto, sto uscendo... \n{errore}")
-        sys.exit()
-    data = s.recv(4096)
-    print(str(data, "utf-8"))
-    invia_comandi(s)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
